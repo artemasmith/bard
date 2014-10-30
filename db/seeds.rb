38f98de587ba20fa1707b0cce03952b856f4da86
kaa <artemasmith@gmail.com>
2014-10-30 22:26:06 +0500

    h4

--- db/seeds.rb
+++ db/seeds.rb
@@ -91,7 +91,7 @@ end
 end
 
 #User.create(email: 'artemasmith@gmail.com', password: '12345678', role: :admin)
-nik = User.new(email: 'nikita@ples.ru', password: '12345678', role: :admin, login: nikita)
+nik = User.new(email: 'nikita@ples.ru', password: '12345678', role: :admin, login: 'nikita')
 nik.save
 nik.activities.create(amount_debet: 100000)
 art = FactoryGirl.create(:user, email: 'artemasmith@gmail.com', password: '12345678', role: :admin, login: 'art')
